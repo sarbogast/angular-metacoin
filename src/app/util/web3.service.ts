9b77f34140a01ff4ec7b1bdfb21f6f1b51c436bd
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2017-10-22 18:37:31 +0200

    First round of simplification on Web3Service - removed the need for WindowRefService altogether - used a window event listener instead of an interval to check whether web3 has been injected - refresh accounts every 100ms without touching web3

--- src/app/util/web3.service.ts
+++ src/app/util/web3.service.ts
@@ -23,7 +23,7 @@ export class Web3Service {
         // Use Mist/MetaMask's provider
         this.web3 = new Web3(window.web3.currentProvider);
       } else {
-        console.log('No web3? You should consider trying MetaMask!')
+        console.log('No web3? You should consider trying MetaMask!');
         // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
         this.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
       }
